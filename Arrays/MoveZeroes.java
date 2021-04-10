678d4636c4f3b473100958d253c118746706507b
levitaxl <hermessamuel10@gmail.com>
2021-04-10 08:58:44 -0400

    new update2

--- Arrays/MoveZeroes.java
+++ Arrays/MoveZeroes.java
@@ -19,6 +19,8 @@ public class MoveZeroes {
                         nums[j]=nums[i];
                         nums[i]=aux;
                         
+                        
+                        
                         break;
                     }
                     
