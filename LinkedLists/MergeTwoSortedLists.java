9ecaf5f569ef77709f95e14ba5d8b51831799121
Levitaxl <hermessamuel10@gmail.com>
2021-04-14 18:30:09 -0400

    .c

--- LinkedLists/MergeTwoSortedLists.java
+++ LinkedLists/MergeTwoSortedLists.java
@@ -21,14 +21,46 @@ public class MergeTwoSortedLists {
     }
      
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode l3 =new ListNode(0);
         
-        ListNode l3 =null;
-        
-        while(l1.next!= null || l2.next!= null){
+        while(l1!= null || l2!= null){
+            
+            if(l1!=null && l2!=null){
+                System.out.println("Entre en el primer if");
+                System.out.println(l1.val+"/////"+l2.val);
+                if(l1.val>=l2.val){
+                    l3.next=l1;
+                    l3.next.next=l2;
+                }
+                else{
+                    l3.next=l2;
+                    l3.next.next=l1;
+                }
+                
+                l1=l1.next;
+                l2=l2.next;
+            }
+            else if(l1==null && l2!=null){
+                System.out.println("Entre en el segundo if");
+                l3.next=l2;
+                l2=l2.next;
+            }
+            else if(l1!=null && l2==null){
+                System.out.println("Entre en el tercer if");
+                l3.next=l1;
+                l1=l1.next;
+            }
+            
+            l3=l3.next;
             
             
         }
-        return l1;
+        return l3;
     }
+    
+     public static void main(String[] args){
+     
+     }
+    
     
 }
